959fe2f3cb64731d6c0b21e183a545031b9333a6
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-18 15:17:12 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 rando = 2
-while rando <= 60000
+while rando <= 100
 
   rando += 1
 
